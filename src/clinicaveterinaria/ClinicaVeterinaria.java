9d7fe8d9f27049198fa406d14e906af246999074
seryeb <seryeb@gmail.com>
2018-05-13 07:44:01 +0200

    Añadida entrada de datos a través de diálogo.

--- src/clinicaveterinaria/ClinicaVeterinaria.java
+++ src/clinicaveterinaria/ClinicaVeterinaria.java
@@ -22,11 +22,15 @@ import java.util.ArrayList;
 
 import javax.swing.JRadioButton;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.JTable;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
 
 public class ClinicaVeterinaria {
 	private JFrame frmClnicaVeterinariaMisif;
@@ -57,8 +61,9 @@ public class ClinicaVeterinaria {
 	/**
 	 * Listas
 	 */
-	private ArrayList<Pajaro> listaPajaros = new ArrayList<>();
+	
 	public class PajaroListModel extends AbstractListModel{
+		private ArrayList<Pajaro> listaPajaros = new ArrayList<>();
 	    @Override
 	    public int getSize() {
 	    	return listaPajaros.size();
@@ -72,7 +77,7 @@ public class ClinicaVeterinaria {
 	        listaPajaros.add(p);
 	        this.fireIntervalAdded(this, getSize(), getSize()+1);
 	    }
-	    public void eliminarPajaros(int index0){
+	    public void eliminarPajaro(int index0){
 	        listaPajaros.remove(index0);
 	        this.fireIntervalRemoved(index0, getSize(), getSize()+1);
 	    }
@@ -80,8 +85,9 @@ public class ClinicaVeterinaria {
 	        return listaPajaros.get(index);
 	    }
 	}
-	private ArrayList<Pez> listaPeces = new ArrayList<>();
+		
 	public class PezListModel extends AbstractListModel{
+		private ArrayList<Pez> listaPeces = new ArrayList<>();
 	    @Override
 	    public int getSize() {
 	    	return listaPeces.size();
@@ -103,8 +109,8 @@ public class ClinicaVeterinaria {
 	        return listaPeces.get(index);
 	    }
 	}
-	private ArrayList<Reptil> listaReptiles = new ArrayList<>();
 	public class ReptilListModel extends AbstractListModel{
+		private ArrayList<Reptil> listaReptiles = new ArrayList<>();
 	    @Override
 	    public int getSize() {
 	    	return listaReptiles.size();
@@ -131,7 +137,7 @@ public class ClinicaVeterinaria {
 	 */
 	private void initialize() {
 		frmClnicaVeterinariaMisif = new JFrame();
-		frmClnicaVeterinariaMisif.setTitle("Cl\u00EDnica Veterinaria");
+		frmClnicaVeterinariaMisif.setTitle("Tienda de Mascotas Misif\u00FA");
 		frmClnicaVeterinariaMisif.setResizable(false);
 		frmClnicaVeterinariaMisif.setBounds(100, 100, 875, 675);
 		frmClnicaVeterinariaMisif.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -148,12 +154,61 @@ public class ClinicaVeterinaria {
 		pajaros.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "P\u00E1jaros", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(51, 51, 51)));
 		pajaros.setLayout(null);
 		
+
+		PajaroListModel pajaro_model = new PajaroListModel();
+		
+		
 		JButton anadirPajaroBoton = new JButton("+");
+		anadirPajaroBoton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				JTextField nombreC = new JTextField();
+				JTextField codigoC = new JTextField();
+				JTextField especieC = new JTextField();
+				JTextField alimentacionC = new JTextField();
+				JTextField observacionesC = new JTextField();
+				JTextField generoC = new JTextField();
+				
+				Object[] message = {
+				    "Nombre:", nombreC,
+				    "Código:", codigoC,
+				    "Especie:", especieC,
+				    "Alimentación:", alimentacionC,
+				    "Observaciones:", observacionesC,
+				    "Género:", generoC,
+				};
+				int option = JOptionPane.showConfirmDialog(frmClnicaVeterinariaMisif, message, "Indique los valores del pájaro", JOptionPane.OK_CANCEL_OPTION);
+				if (option == JOptionPane.OK_OPTION)
+				{
+					Pajaro p = new Pajaro(nombreC.getText(), codigoC.getText(), especieC.getText(), alimentacionC.getText(), observacionesC.getText(),generoC.getText());
+					pajaro_model.addPajaro(p);
+					
+				}
+			}
+		});
+		anadirPajaroBoton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
 		anadirPajaroBoton.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		anadirPajaroBoton.setBounds(105, 25, 49, 41);
 		pajaros.add(anadirPajaroBoton);
 		
+		JList listaPajaros = new JList();
+		listaPajaros.setBounds(32, 86, 234, 199);
+		pajaros.add(listaPajaros);
+		listaPajaros.setModel(pajaro_model);
+		
 		JButton eliminarPajaroBoton = new JButton("-");
+		eliminarPajaroBoton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				int selection = listaPajaros.getSelectedIndex();
+				if (selection!=-1) {
+				   pajaro_model.eliminarPajaro(selection);
+				}
+			}
+		});
 		eliminarPajaroBoton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
@@ -162,13 +217,14 @@ public class ClinicaVeterinaria {
 		eliminarPajaroBoton.setBounds(164, 25, 49, 41);
 		pajaros.add(eliminarPajaroBoton);
 		
+
+		
 		JPanel peces = new JPanel();
 		peces.setBounds(293, 0, 291, 307);
 		superior.add(peces);
 		peces.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "Peces", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(51, 51, 51)));
 		peces.setLayout(null);
-		
-
+				
 		JPanel reptiles = new JPanel();
 		reptiles.setLayout(null);
 		reptiles.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "Reptiles", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(51, 51, 51)));
@@ -179,6 +235,10 @@ public class ClinicaVeterinaria {
 		informacion.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Informaci\u00F3n", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		frmClnicaVeterinariaMisif.getContentPane().add(informacion);
 		informacion.setLayout(null);
+		
+		JLabel label = new JLabel("Nombre:");
+		label.setBounds(29, 41, 63, 28);
+		informacion.add(label);
 	}
 	private class SwingAction extends AbstractAction {
 		public SwingAction() {
@@ -187,5 +247,22 @@ public class ClinicaVeterinaria {
 		}
 		public void actionPerformed(ActionEvent e) {
 		}
+	}
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
 	}
 }
